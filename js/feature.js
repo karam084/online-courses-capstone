87adeda2d1f49e95aaf381325d1957971c6e2b0a
karam084 <karam.ezz84@gmail.com>
2022-02-04 05:34:32 +0200

    Add feature section with javascript

--- js/feature.js
+++ js/feature.js
@@ -8,11 +8,25 @@ const speakers = [
   {
     name: 'Mohamed Samy ',
     image: './src-image/profa.jpg',
-    text: ' Cairo University Vice President for Community Service and Environment Development Affairs',
-    description:
-      'Doctor of Philosophy in Law from Faculty of Pharmacy, Cairo University, June 2000.',
+    text: ' Cairo University Vice President for Community Service',
+    description: 'Doctor of Philosophy in Law from Faculty of Pharmacy',
   },
+  /*{
+    image: './src-image/salma.jpg',
+    name: 'Salima Ikram',
+    text: 'Distinguished University Professor of Egyptology at The American University in Cairo',
+    description:
+      ' worked as an archaeologist in Turkey, Sudan, Greece and the United States',
+  },
+  {
+    image: './src-image/nagw.jpg',
+    name: 'Nagwa Sherif',
+    text: 'Architectural Engineering at The American University in Cairo',
+    description:
+      'She holds a PhD degree from University of Montreal, Canada in 1984',
+  },*/
 ];
+
 speakers.forEach((speaker) => {
   const div = document.querySelector('.speaker-info');
   const dynamicContent = `
@@ -32,7 +46,7 @@ speakers.forEach((speaker) => {
   div.innerHTML += dynamicContent;
 });
 
-const speakers2 = [
+const speaker = [
   {
     image: './src-image/salma.jpg',
     name: 'Salima Ikram',
@@ -48,17 +62,17 @@ const speakers2 = [
       'She holds a PhD degree from University of Montreal, Canada in 1984',
   },
 ];
-speakers2.forEach((speaker1) => {
-  const div = document.querySelector('.col');
+speaker.forEach((speaker1) => {
+  const div = document.querySelector('#hidee');
   const dynamicContent = `
-       <card class="speaker1 ">
-                  <img src=${speaker1.image} alt="Speaker" class="speaker1-img">
-                  <div class="col">
-                    <h3 class="speaker-name1">${speaker1.name}
+       <card class="speaker ">
+                  <img src=${speaker1.image} alt="Speaker" class="speaker-img">
+                  <div class="speaker-info">
+                    <h3 class="speaker-name">${speaker1.name}
                     </h3>
-                    <p class="speaker-desc1">${speaker1.text}
+                    <p class="speaker-desc">${speaker1.text}
                     </p>
-                    <p class="speaker-mor1">${speaker1.description}
+                    <p class="speaker-mor">${speaker1.description}
                     </p>
                   </div>
                 </card>
@@ -66,12 +80,13 @@ speakers2.forEach((speaker1) => {
   `;
   div.innerHTML += dynamicContent;
 });
+
 function showImage() {
   const elems = document.getElementsByClassName('showHide');
   for (let i = 0; i < elems.length; i += 1) {
     if (
-      elems[i].style.visibility === 'visible'
-      || elems[i].style.visibility === ''
+      elems[i].style.visibility === 'visible' ||
+      elems[i].style.visibility === ''
     ) {
       elems[i].style.visibility = 'hidden';
     } else if (elems[i].style.visibility === 'hidden') {
